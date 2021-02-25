1b164168c0af1d3c8c0fa3fa38dc42a419d69cf0
Giuseppe <festagiuseppe89@gmail.com>
2021-02-25 21:33:46 +0100

    Esercizio completato e commentato

---
+++ main.js
@@ -0,0 +1,27 @@
+//Scrivi un programma che stampi i numeri da 1 a 100,
+//ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+//inizio con un ciclo "for" per contare da 1 a 100
+for (var i = 1; i <= 100; i++) {
+
+    //controllo se divisibile per 3 e per 5 allora stampo fizzbuzz
+    if (i% 3 === 0 && i% 5 === 0) { 
+        console.log ("FizzBuzz");
+        document.getElementById("fizz-buzz").innerHTML = "FIZZBUZZ";
+        
+    //controllo se divisibile per 3 e stampo, multipli di 3
+}   else if (i% 3 === 0) {       //prendo il valore "i" e lo divido per "3", resto, confronto il resto con valore "0"
+    console.log ("Fizz");
+    document.getElementById( "fizz").innerHTML = "FIZZ";
+
+    //controllo se divisibile per 5 e stampo, multipli di 5
+}   else if (i % 5 === 0) {      //prendo il valore "i" e lo divido per "5", resto, confronto il resto con valore "0"
+    console.log("Buzz");
+    document.getElementById( "buzz").innerHTML = "BUZZ";
+
+    //altrimenti se non divisibile per 3 o 5 stampo il numero stesso
+}   else {
+    console.log(i);
+    }
+}
